e3af4ea8c18953017779351a69f789410d03d10d
nicnike <57753615+nicnike@users.noreply.github.com>
2023-12-12 13:22:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'listener = listener:main',
+            'listener = listener_package.listener:main',
         ],
     },
 )
